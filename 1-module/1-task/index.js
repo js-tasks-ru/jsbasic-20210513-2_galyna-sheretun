6f136cf0375d8412b2170a31adaa7450f0ee2c5b
GalkaNH <galyna.sheretun@gmail.com>
2021-06-17 05:55:32 +0200

    внесла изменения

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+	let result = 1;
+	while (n) {
+		result *= n--;
+	}
+
+	return result;
+
 }
+
+alert(factorial(1));
+alert(factorial(2));
+alert(factorial(3));
+alert(factorial(4));
+alert(factorial(5));
